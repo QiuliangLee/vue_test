e5821027970fd6ded6382b3f13d2cb09c5fcfd19
liqiuliang <liqiuliang@bilibili.com>
2023-11-28 00:58:31 +0800

    3

--- 44_src_element-ui组件库/App.vue
+++ 44_src_element-ui组件库/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div>
-		<button>原生的按钮</button>
-		<input type="text">
-		<atguigu-row>
-			<el-button>默认按钮</el-button>
-			<el-button type="primary">主要按钮</el-button>
-			<el-button type="success">成功按钮</el-button>
-			<el-button type="info">信息按钮</el-button>
-			<el-button type="warning">警告按钮</el-button>
-			<el-button type="danger">危险按钮</el-button>
-		</atguigu-row>
-		<atguigu-date-picker
-      type="date"
-      placeholder="选择日期">
-    </atguigu-date-picker>
-		<atguigu-row>
-			<el-button icon="el-icon-search" circle></el-button>
-			<el-button type="primary" icon="el-icon-s-check" circle></el-button>
-			<el-button type="success" icon="el-icon-check" circle></el-button>
-			<el-button type="info" icon="el-icon-message" circle></el-button>
-			<el-button type="warning" icon="el-icon-star-off" circle></el-button>
-			<el-button type="danger" icon="el-icon-delete" circle></el-button>
-		</atguigu-row>
-  </div>
+    <div>
+        <button>原生的按钮</button>
+        <input type="text">
+        <atguigu-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </atguigu-row>
+        <atguigu-date-picker
+                placeholder="选择日期"
+                type="date">
+        </atguigu-date-picker>
+        <atguigu-row>
+            <el-button circle icon="el-icon-search"></el-button>
+            <el-button circle icon="el-icon-s-check" type="primary"></el-button>
+            <el-button circle icon="el-icon-check" type="success"></el-button>
+            <el-button circle icon="el-icon-message" type="info"></el-button>
+            <el-button circle icon="el-icon-star-off" type="warning"></el-button>
+            <el-button circle icon="el-icon-delete" type="danger"></el-button>
+        </atguigu-row>
+    </div>
 </template>
 
 <script>
-	export default {
-		name:'App',
-	}
+    export default {
+        name: 'App',
+    }
 </script>
--- 44_src_element-ui组件库/main.js
+++ 44_src_element-ui组件库/main.js
@@ -8,9 +8,8 @@ import App from './App.vue'
 // import ElementUI from 'element-ui';
 //引入ElementUI全部样式
 // import 'element-ui/lib/theme-chalk/index.css';
-
 //按需引入
-import { Button,Row,DatePicker } from 'element-ui';
+import {Button, DatePicker, Row} from 'element-ui';
 
 //关闭Vue的生产提示
 Vue.config.productionTip = false
@@ -23,6 +22,6 @@ Vue.component('atguigu-date-picker', DatePicker);
 
 //创建vm
 new Vue({
-	el:'#app',
-	render: h => h(App),
+    el: '#app',
+    render: h => h(App),
 })
